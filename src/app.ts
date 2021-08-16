5eef26c828d51c3acda3e989a076679acd102c3f
Jakub Krymarys <krymarys.jakub@gmail.com>
2021-08-16 18:22:04 +0200

    Add NGROK and configure it (#3)

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,28 @@
 import express from 'express';
 import cors from 'cors';
+import ngrok from 'ngrok';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 
 import { getCurrentHouseState } from './sensors'
 
 const app = express();
 const port = 5000;
 
+(async function () {
+    const url = await ngrok.connect({
+        authtoken: process.env.AUTH_TOKEN,
+        addr: 5000
+    });
+})();
+
+
 app.get('/get_temp_state', cors() as any, (req, res) => {
     res.send(getCurrentHouseState())
 })
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
-  })
+    console.log(`App listening at http://localhost:${port}`)
+})
